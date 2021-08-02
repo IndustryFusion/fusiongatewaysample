298f2cd9650c5db6cc7c3a6a3804f6485e86d4ab
David Lane <david.lane@iteratec.com>
2021-08-02 10:40:26 +0200

    Initial commit

---
+++ mqttpump/pump.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+i=1
+links=20
+rechts=30
+jahr=35
+monattag=513
+
+
+while true
+do
+    cat > /tmp/message.json <<-EOF
+        {
+            "E2MIX-Flaschendruck_Links": $links,
+            "E2MIX-Flaschendruck_Rechts": $rechts,
+            "Jahr_Folgewartung": $jahr,
+            "MonatTag_Folgewartung": $monattag
+        }
+EOF
+
+    mosquitto_pub -h mqttbroker -p 1883 -t Gasentnahmestelle -f /tmp/message.json
+
+    cat /tmp/message.json
+
+    links=$((links + 1))
+    rechts=$((rechts + 1))
+
+    sleep 2
+done
---
+++ script/transform.ts
@@ -0,0 +1,36 @@
+import { isObject, TransformResult, JSONRoot, JSONObject, isNumber, bcdToInt } from "./util.ts"
+import { datetime } from "./deps.ts"
+
+export function transform(data: JSONRoot): TransformResult {
+    console.log(JSON.stringify(data));
+    if (!isObject(data)) {
+        return { status: 400, statusText: "Only JSON objects supported" };
+    }
+    const objectData = data as JSONObject;
+
+    if ('Jahr_Folgewartung' in objectData && 'MonatTag_Folgewartung' in objectData) {
+        if (!isNumber(objectData['Jahr_Folgewartung'])) {
+            return { status: 400, statusText: "MonatTag_Folgewartung not a number " + objectData['Jahr_Folgewartung'] + " (" + typeof objectData['Jahr_Folgewartung'] + ")" };
+        }
+        if (!isNumber(objectData['MonatTag_Folgewartung'])) {
+            return { status: 400, statusText: "MonatTag_Folgewartung not a number " + objectData['MonatTag_Folgewartung'] + " (" + typeof objectData['MonatTag_Folgewartung'] + ")" };
+        }
+        const jahr = objectData['Jahr_Folgewartung'] as number;
+        const monatTag = objectData['MonatTag_Folgewartung'] as number;
+
+        const monatTagStr = String(bcdToInt(monatTag)).padStart(4, "0");
+        const strDate = monatTagStr.slice(2) + monatTagStr.slice(0,2) + String(bcdToInt(jahr)).padStart(2, "0");
+        objectData['Datum_Folgewartung'] = strDate;
+
+        const maintenanceDate = datetime.parse(strDate, "ddMMyy")
+        const now = new Date();
+
+        let datediffHours = datetime.difference(now, maintenanceDate, { units: ["hours"] }).hours as number;
+        if (maintenanceDate < now) {
+            datediffHours = -datediffHours;
+        }
+        objectData['Stunden_Folgewartung'] = datediffHours;
+    }
+
+    return { status: 201, data: objectData };
+}
